47da1d6a7594483063e55730e9a3cb241956965d
Nar Kumar <njn.kumar@gmail.com>
2019-10-17 09:57:13 -0700

    Storyboard Complete and Working on CollectionView

---
+++ Planets/View Controllers/PlanetsCollectionViewController.swift
@@ -0,0 +1,53 @@
+//
+//  PlanetsCollectionViewController.swift
+//  Planets
+//
+//  Created by Niranjan Kumar on 10/17/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "PlanetCell"
+
+class PlanetsCollectionViewController: UICollectionViewController {
+    
+    var planetController = PlanetController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        return planetController.planets.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+
+}
---
+++ Planets/Views/PlanetCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PlanetCollectionViewCell.swift
+//  Planets
+//
+//  Created by Niranjan Kumar on 10/17/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class PlanetCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+}
